d3c43e76783addb8af608ef8c6ba8a6db09bfb82
eliablm <eliablopez@hotmail.com>
2021-10-05 19:46:46 -0500

    Lee el presupuesto del usuario

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Pregunta from './components/Pregunta';
+
 function App() {
 	return (
 		<div clasname="container">
 			<header>
 				<h1>Presupuesto</h1>
+				<div className="contenido-principal contenido">
+					<Pregunta />
+				</div>
 			</header>
 		</div>
 	);
---
+++ src/components/Pregunta.js
@@ -0,0 +1,41 @@
+import React, { Fragment, useState } from 'react';
+
+const Pregunta = () => {
+	// Definir el state
+	const [cantidad, guardarCantidad] = useState(0);
+
+	// Funcion que lee el presupuesto
+	const definirPresupuesto = (e) => {
+		guardarCantidad(parseInt(e.target.value), 10);
+	};
+
+	// Submit para definir el presupuesto
+	const agregarPresupuesto = (e) => {
+		e.preventDefault();
+
+		// Validar
+
+		// Si se pasa la validación
+	};
+
+	return (
+		<Fragment>
+			<h2>Coloca tu presupuesto</h2>
+			<form onSubmit={agregarPresupuesto}>
+				<input
+					type="number"
+					className="u-full-width"
+					placeholder="Coloca tu presupuesto"
+					onChange={definirPresupuesto}
+				/>
+				<input
+					type="submit"
+					className="button-primary u-full-width"
+					value="Definir Presupuesto"
+				/>
+			</form>
+		</Fragment>
+	);
+};
+
+export default Pregunta;
