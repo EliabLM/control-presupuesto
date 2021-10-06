b5202e3f7dac2e6ab65a83dccc047decb608e3bb
eliablm <eliablopez@hotmail.com>
2021-10-05 22:35:07 -0500

    Agrega carga condicional de componente

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import Error from './Error';
 
-const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
+const Pregunta = ({
+	guardarPresupuesto,
+	guardarRestante,
+	actualizarPregunta,
+}) => {
 	// Definir el state
 	const [cantidad, guardarCantidad] = useState(0);
 	const [error, guardarError] = useState(false);
@@ -25,6 +29,7 @@ const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
 		guardarError(false);
 		guardarPresupuesto(cantidad);
 		guardarRestante(cantidad);
+		actualizarPregunta(false);
 	};
 
 	return (
